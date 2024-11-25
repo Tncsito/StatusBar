a2ab623f37cbf0cbbc09cd36b052347dfeca682c
Tncsito <eduarosantoyo@hotmail.com>
2024-11-24 21:51:14 -0700

    Agregar archivos de proyecto.

---
+++ StatusBar_EESA/StatusBar_EESA.Android/statusbar/StatusBar.cs
@@ -0,0 +1,83 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Plugin.CurrentActivity;
+using StatusBar_EESA.Droid.statusbar;
+using StatusBar_EESA.VistaModelo;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(StatusBar))]
+
+namespace StatusBar_EESA.Droid.statusbar
+{
+    internal class StatusBar : VMstatusbar
+    {
+        WindowManagerFlags _OriginalFlags;
+        Window GetCrurrentwindow()
+        {
+            var window = CrossCurrentActivity.Current.Activity.Window;
+            window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+            return window;
+        }
+        public void MostrarStatusBar()
+        {
+            var activity = (Activity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            attrs.Flags = _OriginalFlags;
+            activity.Window.Attributes = attrs;
+        }
+        public void CambiarColor()
+        {
+            MostrarStatusBar();
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    var currentWindow = GetCrurrentwindow();
+                    currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LayoutStable;
+                    currentWindow.SetStatusBarColor(Android.Graphics.Color.Rgb(18, 18, 18));
+                });
+
+            }
+        }
+        public void OcultarStatusBar()
+        {
+            var activity = (Activity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            _OriginalFlags = attrs.Flags;
+            attrs.Flags |= WindowManagerFlags.Fullscreen;
+            activity.Window.Attributes = attrs;
+        }
+        public void Transparente()
+        {
+            MostrarStatusBar();
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    var currentWindow = GetCrurrentwindow();
+                    currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LayoutFullscreen;
+                    currentWindow.SetStatusBarColor(Android.Graphics.Color.Transparent);
+
+                });
+            }
+        }
+        public void Traslucido()
+        {
+            MostrarStatusBar();
+            var activity = (Activity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            _OriginalFlags = attrs.Flags;
+            attrs.Flags |= WindowManagerFlags.TranslucentStatus;
+            activity.Window.Attributes = attrs;
+        }
+    }
+}
---
+++ StatusBar_EESA/StatusBar_EESA/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using StatusBar_EESA.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StatusBar_EESA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new VMmainpage();
+        }
+    }
+}
